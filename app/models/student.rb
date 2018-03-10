f4226dbf7395f47f63864dc31063f08d905069c2
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-10 05:30:12 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+ def self.search(name)
+   if name == ""
+     self.all
+   elsif name != ""
+     self.all.where("#{:name} LIKE '%#{name}%'")
+   end
+ end
 end
